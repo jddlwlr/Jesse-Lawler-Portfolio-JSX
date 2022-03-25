35ff470827ffc49393562b57434bfa2a62c7f170
Jesse Lawler <jedd.lawler@gmail.com>
2022-03-25 13:59:25 -0600

    updating components and component css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import "./header.css";
+import { BsLinkedin, BsGithub, BsMailbox } from "react-icons/bs";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Jesse Lawler</h1>
+        <h5>Full Stack Developer</h5>
+        <div className="callto">
+          <a href="#contact" className="btn">
+            Contact
+          </a>
+          <a href="/src/assets/JesseLawlerResume.pdf" download className="btn">
+            Download Resume
+          </a>
+        </div>
+        <div className="HeaderSocials">
+          <a
+            href="https://linkedin.com/in/jddlwlr"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsLinkedin size={30} />
+          </a>
+          <a href="https://github.com/jddlwlr" target="_blank" rel="noreferrer">
+            <BsGithub size={30} />
+          </a>
+          <a href="mailto:jedd.lawler@gmail.com?subject= Saw your portfolio!">
+            <BsMailbox size={30} />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./nav.css";
+import { IoHome, IoBody, IoChatbubble, IoTerminal } from "react-icons/io5";
 
 const Nav = () => {
-  return <div>nav</div>;
+  return (
+    <nav>
+      <a href="#asad">
+        <IoHome />
+      </a>
+      <a href="#about">
+        <IoBody />
+      </a>
+      <a href="#contact">
+        <IoChatbubble />
+      </a>
+      <a href="#projects">
+        <IoTerminal />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
